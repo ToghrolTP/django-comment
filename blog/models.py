21e40157b5f992563e1800da04a4fadb151e8b06
ToghrolTP <toghroltheprogrammer@gmail.com>
2023-12-23 17:49:31 +0330

    Add comment model

--- blog/models.py
+++ blog/models.py
@@ -29,3 +29,14 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    body = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
