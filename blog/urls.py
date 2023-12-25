f554d06ed88288fbae4102dddfb06b2e04b9b5ab
ToghrolTP <toghroltheprogrammer@gmail.com>
2023-12-25 12:53:22 +0330

    Add logic for comment

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("posts/<slug:slug>", views.post_list, name="post_list")
+    path("posts/<slug:slug>", views.post_detail, name="post_detail")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post, Comment
+from .forms import CommentForm
 
 
 def home(request):
@@ -11,10 +12,23 @@ def home(request):
     return render(request, "blog/home.html", context)
 
 
-def post_list(request, slug):
-    post = Post.objects.get(slug=slug)
+def post_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    comments = Comment.objects.filter(post=post)
+
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect("post_detail", slug=post.slug)
+    else:
+        form = CommentForm()
 
     context = {
         "post": post,
+        "comments": comments,
+        "form": form,
     }
-    return render(request, "blog/post_list.html", context)
+    return render(request, "blog/post_detail.html", context)
